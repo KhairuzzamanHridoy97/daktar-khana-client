9db262b2e0735cfda6467664a2c97cbe3193508f
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-03-16 10:06:08 +0600

    calendar added in appointheader from shared

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+import chair from '../../../images/chair.png'
+import Calendar from '../../Shared/Calendar/Calendar';
+
 
 const AppointmentHeader = () => {
     return (
-        <div>
-            <h3>Appointment Header</h3>
-        </div>
+       <Container >
+           <Grid container spacing={2}>
+
+           <Grid item xs={12} md={6}>
+                <Calendar></Calendar>
+           </Grid>
+           <Grid item xs={12} md={6}>
+                <img src={chair} style={{width:'100%'}} alt="" />
+           </Grid>
+           </Grid>
+       </Container>
     );
 };
 
